f53f25642e81e668fe23c0bdb079497243d97734
Aleksandr12234 <566566sana@gmail.com>
2024-11-02 08:12:01 +0600

    Add files via upload

---
+++ c#27_09/Program1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program1{
+    static void Main(){
+        //количество локальных минимумов
+        Console.WriteLine("Введите количество элемнтов:");
+        int n=Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введите элемент:");
+        int a1=Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите элемент:");
+        int a2=Convert.ToInt32(Console.ReadLine());
+        int k=0;
+
+        for(int i=2; i<n; i++){
+            Console.WriteLine("Введите элемент:");
+            int a3=Convert.ToInt32(Console.ReadLine());
+            if(a1>a2 && a2<a3){
+                k++;
+            }
+            a1=a2;
+            a2=a3;
+        }
+
+        Console.WriteLine("ответ: "+k);
+        }
+    }
---
+++ c#27_09/Program4.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program4{
+        static void Main(){
+            //макс кол-во четных элементов подрят
+            Console.WriteLine("Введите количество элемнтов:");
+            int n=Convert.ToInt32(Console.ReadLine());
+
+            int k=0;
+            int max=0;
+
+            for(int i=0; i<n; i++){
+                Console.WriteLine("Введите элемент:");
+                int a=Convert.ToInt32(Console.ReadLine());
+                
+                if(Math.Abs(a)%2==0){
+                    k++;
+                    max=Math.Max(k, max);
+                }else k=0;
+  
+            }
+
+            Console.WriteLine("ответ: "+max);
+        }
+    }
