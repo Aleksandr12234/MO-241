a14ffaee55e2aedd3dc4e27cd5bfad023a656070
Aleksandr12234 <566566sana@gmail.com>
2024-11-17 16:03:11 +0600

    Add files via upload

---
+++ c#08_11/Program1.cs
@@ -0,0 +1,63 @@
+﻿using System.Diagnostics.Metrics;
+
+internal class Program1
+{
+    private static void Main(string[] args)
+    {
+        //определить пары номеров столбцов в которых стоят одинаковые элементы (сортировать нельзя)
+        Console.Write("Введите количество строк: ");
+        int n=Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите количество столбцов: ");
+        int m=Convert.ToInt32(Console.ReadLine());
+
+        int[,] array=new int[n,m];
+
+        for(int i=0; i<m; i++){
+            Console.WriteLine($"столбец {i+1}:");
+            for(int k=0; k<n; k++){
+                array[k,i]=Convert.ToInt32(Console.ReadLine());      
+            }
+        }
+
+        Console.WriteLine("Введёный массив:");
+        for(int i=0; i<n; i++){
+            for(int k=0; k<m; k++){
+                Console.Write(array[i,k]+" ");
+            }
+            Console.WriteLine();
+        }
+
+
+        for(int k=0; k<m; k++){
+            int sum1=0;
+            int p1=1;
+            int zeroCounte=0;
+            for(int i=0; i<n; i++){
+                sum1+=array[i,k];
+                p1*=array[i,k]==0 ? 1:array[i,k];
+                if(array[i,k]==0)zeroCounte+=1;
+            }
+
+            Console.Write("одинаковые с "+(k+1)+":");
+
+            bool b=false;
+            for(int k1=0; k1<m; k1++){
+                if(k1==k)continue;
+                int sum2=0;
+                int p2=1;
+                int zeroCounte2=0;
+                for(int i=0; i<n; i++){
+                    sum2+=array[i,k1];
+                    p2*=array[i,k1]==0 ? 1:array[i,k1];
+                    if(array[i,k1]==0)zeroCounte2+=1;
+                }
+                if(sum1==sum2 && p1==p2 && zeroCounte==zeroCounte2){
+                    Console.Write(" "+(k1+1));
+                    b=true;
+                }
+            }
+            Console.WriteLine(b ? "":" нету");
+        }
+
+    }
+}
---
+++ c#08_11/Program3.cs
@@ -0,0 +1,47 @@
+﻿internal class Program3
+{
+    private static void Main(string[] args)
+    {
+        //опр номера строк элементы которых элементы которых образуют равномерно убывающую последовательность
+        
+        Console.Write("Введите количество строк: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите количество столбцов: ");
+        int m = Convert.ToInt32(Console.ReadLine());
+
+        double[,] array = new double[n, m];
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine($"строка {i + 1}:");
+            for (int k = 0; k < m; k++)
+            {
+                array[i,k] = Convert.ToDouble(Console.ReadLine());
+            }
+        }
+
+        Console.WriteLine("Введёный массив:");
+        for (int i = 0; i < n; i++)
+        {
+            for (int k = 0; k < m; k++)
+            {
+                Console.Write(array[i, k] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        
+        string s="";
+        for(int i=0; i<n; i++){
+            double b=array[i,1]-array[i,0];
+            double q=array[i,1]/array[i,0];
+            if(b>=0)continue;
+            else if(m==2) s+=" "+(i+1);
+            for(int k = 2; k<m && (array[i,k]-array[i,k-1]==b || array[i,k]/array[i,k-1]==q); k++){
+                if(k==m-1)s+=" "+(i+1);
+            }
+        }
+
+        Console.WriteLine("Ответ:" + (s==""? "нету":s));
+    }
+}
